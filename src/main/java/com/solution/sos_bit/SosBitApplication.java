ddb0097ee66fc807aedf17446a83c80f28bc2ebe
Lucas Amorim <lucas.de.amorimarq@gmail.com>
2024-11-27 20:47:32 -0300

    Initial commit

---
+++ src/main/java/com/solution/sos_bit/SosBitApplication.java
@@ -0,0 +1,13 @@
+package com.solution.sos_bit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosBitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosBitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solution/sos_bit/SosBitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solution.sos_bit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosBitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
